6a769f9253788311aff134d48c99b2a81c777c35
Mike <Autapomorph@users.noreply.github.com>
2018-09-15 16:58:45 +0300

    add timezonedb config

--- src/config/weather/index.js
+++ src/config/weather/index.js
@@ -1,4 +1,4 @@
-export const OWM_API_BASE = 'api.openweathermap.org';
+export const OWM_API = 'api.openweathermap.org';
 export const OWM_API_PROTOCOL = 'https://';
 export const OWM_API_VERSION = '2.5';
 export const OWM_API_KEY = '0cc1e365769e7138bbcfd2a60fda214d';
@@ -12,6 +12,6 @@ export const OWM_API_CITY_NAME_QUERY_PARAM = `q`;
 export const OWM_API_LATITUDE_QUERY_PARAM = `lat`;
 export const OWM_API_LONGITUDE_QUERY_PARAM = `lon`;
 
-export const OWM_API_WEATHER_BASE = `${OWM_API_PROTOCOL}${OWM_API_BASE}/data/${OWM_API_VERSION}`;
-export const OWM_API_WEATHER_CITY = `${OWM_API_WEATHER_BASE}/weather`;
-export const OWM_API_WEATHER_SEARCH = `${OWM_API_WEATHER_BASE}/find`;
+export const OWM_API_BASE = `${OWM_API_PROTOCOL}${OWM_API}/data/${OWM_API_VERSION}`;
+export const OWM_API_WEATHER_CITY = `${OWM_API_BASE}/weather`;
+export const OWM_API_WEATHER_SEARCH = `${OWM_API_BASE}/find`;
