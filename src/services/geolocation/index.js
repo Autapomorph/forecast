3c78d8e8a4c28be66bd1473900b236285bc23caa
Autapomorph <Autapomorph@users.noreply.github.com>
2018-09-16 20:45:33 +0300

    add permission api support for geolocation

--- src/services/geolocation/index.js
+++ src/services/geolocation/index.js
@@ -1,10 +1,41 @@
 import { IPAPI_API } from '../../config/geolocation';
 
 export default class GeolocationService {
-  static fetchGeolocation = () =>
-    new Promise((resolve, reject) => {
+  static getGeoPermissionState() {
+    return new Promise((resolve, reject) => {
+      if (navigator.permissions) {
+        navigator.permissions
+          .query({ name: 'geolocation' })
+          .then(permission => resolve(permission.state));
+      } else {
+        reject(new Error('Permission API is not supported'));
+      }
+    });
+  }
+
+  static fetchGeolocationWithPermission = async () => {
+    try {
+      const geoPermissionState = await GeolocationService.getGeoPermissionState();
+
+      if (geoPermissionState === 'denied') {
+        return GeolocationService.fetchGeolocationByIP();
+      }
+
+      return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+    } catch (e) {
+      return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+    }
+  };
+
+  static fetchGeolocation() {
+    return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(resolve, reject);
     });
+  }
 
   static fetchGeolocationByIP = async () => {
     const response = await fetch(IPAPI_API);
