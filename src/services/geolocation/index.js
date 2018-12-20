4cc82814e4072416f88946dd89344d3381150767
Autapomorph <Autapomorph@users.noreply.github.com>
2018-12-20 20:31:16 +0300

    add geolocation error codes i18n

--- src/services/geolocation/index.js
+++ src/services/geolocation/index.js
@@ -1,42 +1,27 @@
 import { IPAPI_API } from '../../config/geolocation';
 
 export default class GeolocationService {
-  static getGeoPermissionState() {
-    return new Promise((resolve, reject) => {
-      if (navigator.permissions) {
-        navigator.permissions
-          .query({ name: 'geolocation' })
-          .then(permission => resolve(permission.state));
-      } else {
-        reject(new Error('messages.errors.permissions.APIUnavailable'));
-      }
+  static fetchGeolocation = async () =>
+    new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, ({ code }) => {
+        switch (code) {
+          case 1:
+            return reject(new Error('messages.errors.geolocation.permissionDenied'));
+          case 2:
+            return reject(new Error('messages.errors.geolocation.positionUnavailable'));
+          case 3:
+            return reject(new Error('messages.errors.geolocation.timeout'));
+          default:
+            return reject(new Error('messages.errors.geolocation.positionUnavailable'));
+        }
+      });
     });
-  }
-
-  static fetchGeolocation = async () => {
-    const geoPermissionState = await GeolocationService.getGeoPermissionState();
-
-    /*
-      browsers do not show quick settings switcher if permission state is 'denied',
-      so it might be better disable permission check and only rely on results of
-      `navigator.geolocation.getCurrentPosition` method
-    */
-    if (geoPermissionState === 'denied') {
-      throw new Error('messages.errors.geolocation.geoDenied');
-    }
-
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resolve, () =>
-        reject(new Error('messages.errors.geolocation.geoDenied')),
-      );
-    });
-  };
 
   static fetchGeolocationByIP = async () => {
     const response = await fetch(IPAPI_API);
 
     if (!response.ok) {
-      throw new Error('messages.errors.geolocation.geoIPFailed');
+      throw new Error('messages.errors.geolocation.geoIPFetchFailed');
     }
 
     const geolocationData = await response.json();
