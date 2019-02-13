5b95f307d392fdb2711f6c203fbf3ee4bf6d4bea
Autapomorph <Autapomorph@users.noreply.github.com>
2019-02-13 21:44:53 +0300

    update rescripts config

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -1,1 +1,10 @@
-module.exports = [['use-babel-config', 'babel.config.js']];
+module.exports = [
+  ['use-babel-config', 'babel.config.js'],
+  {
+    jest: config => {
+      // eslint-disable-next-line no-param-reassign
+      config.transform['^.+\\.(js|jsx|ts|tsx)$'] = 'babel-jest';
+      return config;
+    },
+  },
+];
