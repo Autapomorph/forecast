99e16662c4af967c371ae2e6db96711ac00fda7e
Autapomorph <Autapomorph@users.noreply.github.com>
2019-04-12 21:49:33 +0300

    add wind card dir enum

--- src/models/weather/wind.ts
+++ src/models/weather/wind.ts
@@ -1,7 +1,25 @@
-// eslint-disable-next-line import/prefer-default-export
 export interface IWindFormat {
   bearing: number;
   speed: number;
   cardDir: string;
   icon: string;
+}
+
+export enum WindCardDir {
+  N = 'N',
+  NNE = 'NNE',
+  NE = 'NE',
+  ENE = 'ENE',
+  E = 'E',
+  ESE = 'ESE',
+  SE = 'SE',
+  SSE = 'SSE',
+  S = 'S',
+  SSW = 'SSW',
+  SW = 'SW',
+  WSW = 'WSW',
+  W = 'W',
+  WNW = 'WNW',
+  NW = 'NW',
+  NNW = 'NNW',
 }
--- src/utils/weatherData/wind/mapDegToCardDir.ts
+++ src/utils/weatherData/wind/mapDegToCardDir.ts
@@ -1,4 +1,6 @@
-export default function mapDegToCardDir(deg: number): string {
+import { WindCardDir as D } from '../../../models/weather/wind';
+
+export default function mapDegToCardDir(deg: number): D {
   if (Number.isNaN(deg)) {
     throw new Error('"deg" param is not a number');
   }
@@ -9,37 +11,37 @@ export default function mapDegToCardDir(deg: number): string {
     deg = deg / rate - 360;
   }
 
-  if ((deg >= 0 && deg <= 11.25) || (deg >= 348.75 && deg <= 360)) return 'N';
+  if ((deg >= 0 && deg <= 11.25) || (deg >= 348.75 && deg <= 360)) return D.N;
 
-  if (deg >= 11.25 && deg <= 33.75) return 'NNE';
+  if (deg >= 11.25 && deg <= 33.75) return D.NNE;
 
-  if (deg >= 33.75 && deg <= 56.25) return 'NE';
+  if (deg >= 33.75 && deg <= 56.25) return D.NE;
 
-  if (deg >= 56.25 && deg <= 78.75) return 'ENE';
+  if (deg >= 56.25 && deg <= 78.75) return D.ENE;
 
-  if (deg >= 78.75 && deg <= 101.25) return 'E';
+  if (deg >= 78.75 && deg <= 101.25) return D.E;
 
-  if (deg >= 101.25 && deg <= 123.75) return 'ESE';
+  if (deg >= 101.25 && deg <= 123.75) return D.ESE;
 
-  if (deg >= 123.75 && deg <= 146.25) return 'SE';
+  if (deg >= 123.75 && deg <= 146.25) return D.SE;
 
-  if (deg >= 146.25 && deg <= 168.75) return 'SSE';
+  if (deg >= 146.25 && deg <= 168.75) return D.SSE;
 
-  if (deg >= 168.75 && deg <= 191.25) return 'S';
+  if (deg >= 168.75 && deg <= 191.25) return D.S;
 
-  if (deg >= 191.25 && deg <= 213.75) return 'SSW';
+  if (deg >= 191.25 && deg <= 213.75) return D.SSW;
 
-  if (deg >= 213.75 && deg <= 236.25) return 'SW';
+  if (deg >= 213.75 && deg <= 236.25) return D.SW;
 
-  if (deg >= 236.25 && deg <= 258.75) return 'WSW';
+  if (deg >= 236.25 && deg <= 258.75) return D.WSW;
 
-  if (deg >= 258.75 && deg <= 281.25) return 'W';
+  if (deg >= 258.75 && deg <= 281.25) return D.W;
 
-  if (deg >= 281.25 && deg <= 303.75) return 'WNW';
+  if (deg >= 281.25 && deg <= 303.75) return D.WNW;
 
-  if (deg >= 303.75 && deg <= 326.25) return 'NW';
+  if (deg >= 303.75 && deg <= 326.25) return D.NW;
 
-  if (deg >= 326.25 && deg <= 348.75) return 'NNW';
+  if (deg >= 326.25 && deg <= 348.75) return D.NNW;
 
   throw new Error('Invalid wind direction');
 }
