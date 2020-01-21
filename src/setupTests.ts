2ad8154caab13d28a31a7d4c9984d853ab2afd86
Autapomorph <Autapomorph@users.noreply.github.com>
2020-01-21 15:07:27 +0300

    fix ts tests
    - add `React` explicit import
    - remove globals from `setupTests.ts` due to ts import ignore
    - update deps

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,18 +1,4 @@
-import React from 'react';
-import Enzyme, { shallow, mount, render } from 'enzyme';
+import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
-interface Global extends NodeJS.Global {
-  React: typeof React;
-  shallow: typeof shallow;
-  mount: typeof mount;
-  render: typeof render;
-}
-
-declare let global: Global;
-global.React = React;
-global.shallow = shallow;
-global.mount = mount;
-global.render = render;
 
 Enzyme.configure({ adapter: new Adapter() });
